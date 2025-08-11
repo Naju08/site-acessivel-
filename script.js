0f56f0ec2764a271d3da85111e1a3e70056bbe73
Naju08 <0000108798984xsp@al.educacao.sp.gov.br>
2025-08-11 10:21:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     })
 })
+botaoDeAcessibilidade.addEventListener('click', function (){
+ botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+ opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+})
